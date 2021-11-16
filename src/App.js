efbabdced37a38f1f45c99789202e2ae9500fa83
madmanforces <hwangcc1019@naver.com>
2021-11-17 01:28:44 +0900

    style components black/white mode

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 48px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>🤩</span>
-      </Box>
+     <Title>Hello</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+
+const darkTheme = {
+  textColor: "white",
+  backgroundColor: "black",
+};
+
+const lightTheme = {
+  textColor: "black",
+  backgroundColor: "white",
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
