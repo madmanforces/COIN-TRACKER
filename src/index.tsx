f1455ecef5c199a8ef25713cccfd6cbc3b828ed2
madmanforces <hwangcc1019@naver.com>
2021-11-18 00:35:57 +0900

    typeScript practice

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 
-const darkTheme = {
-  textColor: "white",
-  backgroundColor: "black",
-};
 
-const lightTheme = {
-  textColor: "black",
-  backgroundColor: "white",
-};
 
 
 ReactDOM.render(
   <React.StrictMode>
-     <ThemeProvider theme={darkTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
