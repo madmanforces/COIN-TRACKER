03da652e32aea801e9fbf6029050e7e06b3f4e3c
madmanforces <hwangcc1019@naver.com>
2021-11-19 23:05:26 +0900

    typeScript styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
 
 
 
@@ -8,7 +10,9 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+     <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    bgColor: "#ffda79",
+    textColor: "#34ace0",
+    accentColor: "#84817a",
+  };
