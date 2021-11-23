25fe69e11d61ec41fef34af9f4ebff5aa43f25da
madmanforces <hwangcc1019@naver.com>
2021-11-23 21:34:14 +0900

    typeScript practice coin imformation app

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -23,7 +23,8 @@ const Coin = styled.li`
   a {
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
+    display: flex;
+    align-items: center;
   }
   &:hover {
     a {
@@ -33,50 +34,63 @@ const Coin = styled.li`
 `;
 const Title = styled.h1`
   font-size: 48px;
-  color: ${(props) => props.theme.accentColor};
+  color: #f5f6fa;
+  font-weight: 700;
 `;
 const Loader = styled.span`
   text-align: center;
   display: block;
 `;
 
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
 interface CoinInterface {
-  id: string;
-  name: string;
-  symbol: string;
-  rank: number;
-  is_new: boolean;
-  is_active: boolean;
-  type: string;
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
 }
 function Coins() {
     const [coins, setCoins] = useState<CoinInterface[]>([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-      (async () => {
-        const response = await fetch("https://api.coinpaprika.com/v1/coins");
-        const json = await response.json();
-        setCoins(json.slice(0, 100));
-        setLoading(false);
-      })();
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
     }, []);
     return (
-      <Container>
-        <Header>
-          <Title>코인</Title>
-        </Header>
-        {loading ? (
-        <Loader>Loading...</Loader>
-      ) : (
-        <CoinsList>
-          {coins.map((coin) => (
-            <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-            </Coin>
-          ))}
-        </CoinsList>
-      )}
-    </Container>
-  );
+        <Container>
+            <Header>
+                <Title>순재코인</Title>
+            </Header>
+            {loading ? (
+                <Loader>순재코인떡상중</Loader>
+            ) : (
+                <CoinsList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`}>
+                                <Img
+                                src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                                />
+                                {coin.name} &rarr;
+                                </Link>
+
+                        </Coin>
+                    ))}
+                </CoinsList>
+            )}
+        </Container>
+    );
 }
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-    bgColor: "#ffda79",
-    textColor: "#34ace0",
+    bgColor: "#54a0ff",
+    textColor: "#2d3436",
     accentColor: "#84817a",
   };
