7a956393a13b43b9f70c7974517bbd355baa7c8f
madmanforces <hwangcc1019@naver.com>
2021-12-01 00:09:39 +0900

    typeScript practice coin imfomation react query dev-tool add

--- src/routes/api.tsx
+++ src/routes/api.tsx
@@ -1,5 +1,18 @@
+const BASE = `https://api.coinpaprika.com/v1`;
+
+
 export function fetchCoins() {
-    return fetch("https://api.coinpaprika.com/v1/coins").then((response) =>
-      response.json()
+    return fetch(`${BASE}/coins`).then((response) => response.json()
     );
   }
+
+
+export function fetchCoinInfo(coinId=String) {
+    return fetch(`${BASE}/Coins/${coinId}`).then((response) => response.json()
+    );
+}
+
+export function fetchPriceInfo(coinId=String) {
+    return fetch(`${BASE}/Coins/${coinId}`).then((response) => response.json()
+    );
+}
