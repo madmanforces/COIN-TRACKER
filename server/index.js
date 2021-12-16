4013ef576d19ef5a45a61595cfd26330c3622689
madmanforces <hwangcc1019@naver.com>
2021-12-16 20:50:49 +0900

    db secret

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 6000;
+const PORT = 3002;
 const {User} = require("./models/User");
 const bodyParser = require('body-parser')
+
+const config = require('./key')
 
 //APPICATION
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,12 +13,12 @@ app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://anthony:tnswo12@cluster0.gczx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     
 }).then(() => console.log('MongoDB Connect'))
   .catch(err => console.log('MongoDB DisConnect'));
 
-app.get('/',(req,res) => res.send('hi im dev'));
+app.get('/',(req,res) => res.send('hi im '));
 
 app.post('/register', (req, res) => {
   //화원가입 필요 정보들을 client에서 가져오면 그것들을
