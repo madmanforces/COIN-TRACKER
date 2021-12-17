36aaff60044f147bf6400a9fdfe78fb2e64208b2
madmanforces <hwangcc1019@naver.com>
2021-12-17 20:17:07 +0900

    dkdk

---
+++ src/routes/Login/Login.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {  Form, Label, Input, Button, Header, Title } from '../SignUp/style'
+import { Link,} from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+const LinkContainer = styled.p`
+margin:  auto ;
+padding-left: 45px;
+font-size: 15px;
+color: #616061;
+width: 400px;
+max-width: 400px;
+& a {
+  color: #1264a3;
+  text-decoration: none;
+  font-weight: 700;
+  &:hover {
+    text-decoration: underline;
+  }
+}
+`;
+
+
+
+
+function LogIn (props) { 
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+      setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+      setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+      event.preventDefault();
+
+      let body = {
+          email: Email,
+          password: Password
+      }
+
+      axios.post('http://localhost:3002/api/users/login',body)
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push('/')
+                } else {
+                    alert('Error˝')
+                }
+            })
+
+
+  }
+  return (
+    <div id="container">
+      <Header><Title>Login</Title></Header>
+      <Form onSubmit={onSubmitHandler}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={Email} onChange={onEmailHandler} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={Password} onChange={onPasswordHandler} />
+          </div>
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signUp">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ src/routes/SignUp/SignUp.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from "react-router-dom";
+import {Header,Title,Form,Input,Button,LinkContainer,Error,Success,Label} from "./style"
+import axios from "axios";
+import { registerUser } from '../_actions/user_action';
+
+
+
+function SignUp(props) {
+    
+   
+
+    const [Email, setEmail] = useState("")
+    const [NickName, setNickName] = useState("")
+    const [Password, setPassword] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onNickNameHandler = (event) => {
+        setNickName(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if (Password !== ConfirmPassword) {
+            return alert('비밀번호와 비밀번호 확인은 같아야 합니다.')
+        }
+
+        let body = {
+            email: Email,
+            password: Password,
+            nickname: NickName
+        }
+        axios.post('http://localhost:3002/api/users/register',body)
+        .then(response => {
+            if (response.payload.loginSuccess) {
+                props.history.push('/')
+            } else {
+                alert('Error˝')
+            }
+        })
+    }
+  return (
+    <div id="container">
+      <Header><Title>Sign Up</Title></Header>
+      <Form onSubmit={onSubmitHandler}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={Email} onChange={onEmailHandler} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={NickName} onChange={onNickNameHandler} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={Password} onChange={onPasswordHandler} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={ConfirmPassword}
+              onChange={onConfirmPasswordHandler}
+            />
+          </div>
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <Link to="/Login">로그인 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp ;
